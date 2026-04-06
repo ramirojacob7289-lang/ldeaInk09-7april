259543869d2a4adf4bac9f065738b2136a2a0af4
ramirojacob7289-lang <ramirojacob7289@gmail.com>
2026-04-07 00:46:22 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,18 @@
 <!--[if IE 8]> <html class="no-js lt-ie9" lang="en"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
 	<head>
+		<!-- Histats.com  START  (aync)-->
+<script type="text/javascript">var _Hasync= _Hasync|| [];
+_Hasync.push(['Histats.start', '1,5018962,4,0,0,0,00010000']);
+_Hasync.push(['Histats.fasi', '1']);
+_Hasync.push(['Histats.track_hits', '']);
+(function() {
+var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+hs.src = ('//s10.histats.com/js15_as.js');
+(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+})();</script>
+<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?5018962&101" alt="web counter" border="0"></a></noscript>
+<!-- Histats.com  END  -->
 		
 		<title>Rainbow HTML5 Template</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
